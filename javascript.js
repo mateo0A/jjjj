10cf0cf348dc0fadad0c882331a2797ed5ef9037
mateo0A <hg097870pq@gmail.com>
2025-01-01 20:09:22 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let body=document.querySelector("body"),
+butt=document.querySelector("div")
+
+butt.addEventListener("mouseover",()=>{
+    body.style.background="#000"
+})
+butt.addEventListener("mouseleave",()=>{
+    body.style.background="#FFF"
+})
